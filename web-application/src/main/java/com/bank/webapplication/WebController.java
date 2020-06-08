757e918edfc2c4eeba0a8390a9a60b2cfc6d1501
eswarap <eswarap@outlook.in>
2020-06-08 16:16:53 +0530

    Controller redirected to account service api

--- web-application/src/main/java/com/bank/webapplication/WebController.java
+++ web-application/src/main/java/com/bank/webapplication/WebController.java
@@ -3,7 +3,10 @@ package com.bank.webapplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WebController {
@@ -16,10 +19,15 @@ public class WebController {
         return "home";
     }
 
-    @GetMapping("/accounts")
+    /*@GetMapping("/accounts")
     String all(ModelMap modelMap) {
         modelMap.put("accounts", webAccountsService.getAllAccounts());
         return "accounts";
+    }*/
+
+    @RequestMapping("/accounts")
+    public List<Account> getAccounts(){
+        return webAccountsService.getAllAccounts();
     }
 
 }
